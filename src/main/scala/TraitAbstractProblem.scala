82e94c2ca5d3a5ec24fa0f6cefe42885b4bd98b1
Fuma_Tsuchiya <korezonzip@gmail.com>
2020-06-14 15:24:25 +0900

    done Try, Either

---
+++ src/main/scala/TraitAbstractProblem.scala
@@ -0,0 +1,12 @@
+object TraitAbstractProblem {
+  def main(args: Array[String]): Unit = {
+    ???
+  }
+
+  abstract class Calc(
+    height: Int
+  ) {
+    def calculate(): Int
+  }
+  //class Squeal extends Calc(height = 10)
+}
