b662e04f92c2e60a986a379d69dbb219fa48d2fa
Fuma_Tsuchiya <korezonzip@gmail.comm>
2020-07-05 22:51:56 +0900

    done question9-14

--- src/main/scala/AdvanceProblem.scala
+++ src/main/scala/AdvanceProblem.scala
@@ -1,7 +1,10 @@
+import scala.concurrent.Future
+import scala.util.{Failure, Success, Try}
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.{Await, Future}
+
 object AdvanceProblem {
   def main(args: Array[String]): Unit = {
-    val nums    = Seq(3, 5, 1, 0)
-    val numsOpt = Seq(Some(3), Some(1), None, Some(10))
     println(question1(Some(8)))
     println(question2(Some(3)))
     println(question3(nums))
@@ -12,7 +15,41 @@ object AdvanceProblem {
     println(question7(nums))
     println(question7_2(nums))
     println(question8(nums))
+    println(question9(articles, articlesDetail))
+    question10(Set(1,2,3,4), Set(3,4,5,6,6))
+    println(question11(people))
+    println(question11_2(people))
+    println(question12(numStr))
+    question13(fOptStr)
+    question13(fNoneStr)
+    question13_2(fOptStr)
+    question13_2(fNoneStr)
+    question14(fEithR)
+    question14(fEithL)
   }
+  val nums    = Seq(3, 5, 1, 0)
+  val numsOpt = Seq(Some(3), Some(1), None, Some(10))
+  val articles: Seq[Article] =
+    Seq(
+      Article(1, "フロント", "html, cssの修正"),
+      Article(2, "バック", "Scalaだよ"),
+      Article(3, "インフラ", "AWS")
+    )
+  val articlesDetail: Seq[ArticleDetail] =
+    Seq(
+      ArticleDetail(1, "done"),
+      ArticleDetail(2, "done"),
+    )
+  val people = Seq(
+    Person(1, "太郎", Some("male")),
+    Person(2, "Rin", Some("female")),
+    Person(3, "Pochi", None)
+  )
+  val numStr = "1,2,3,hello,4"
+  val fOptStr = Future(Some("ほげ"))
+  val fNoneStr = Future(None)
+  val fEithR  = Future(Right("成功"))
+  val fEithL  = Future(Left(199))
 
   //Q1
   //Option[Int]型の numOptを引数として受け取り
@@ -78,6 +115,85 @@ object AdvanceProblem {
 
   def question8(nums: Seq[Int]) = {
     nums.distinct
+  }
+
+  case class Article(
+    id:    Long,
+    title: String,
+    body:  String
+  )
+  case class ArticleDetail(
+    articleId: Long,
+    category:  String
+  )
+  def question9(articles: Seq[Article], articleDetails: Seq[ArticleDetail] )=  {
+    articles.map(x => articleDetails.find(_.articleId == x.id))
+  }
+
+  //10
+  def question10(set1: Set[Int], set2: Set[Int]) = {
+    println("差集合は"+ set1.diff(set2))
+    println("和集合は"+ set1.union(set2))
+  }
+
+  //11
+  case class Person(
+    id: Long,
+    name: String,
+    gender: Option[String]
+  )
+
+  //11
+  def question11(people: Seq[Person]) = {
+    people.map(x => Person(x.id, x.name, None))
+  }
+  def question11_2(people: Seq[Person]) = {
+    people.map(_.copy(gender = None))
+  }
+
+  //12
+  def question12(numStr: String) = {
+    numStr.split(",").map(_.toIntOption)
+  }
+
+  def question12_2(numStr: String) = {
+    numStr.split(",").map(x =>
+      Try(x.toInt).toOption
+    )
+  }
+
+  //13
+  def question13(fOptStr: Future[Option[String]]) = {
+    fOptStr.onComplete{
+      case Success(value) => value match {
+        case Some(x) => println(s"onComplete : ${x}")
+        case None    => println("なしだ")
+      }
+      case Failure(exception) => println(s"エラー: ${exception}")
+    }
+  }
+
+  def question13_2(fOptStr: Future[Option[String]]) = {
+    for {
+      optStr <- fOptStr
+    } yield {
+      optStr match {
+        case Some(x) => println(x)
+        case None    => println("なしNormal")
+      }
+    }
+  }
+
+  //14
+  def question14(fEith: Future[Either[Int, String]]) = {
+    for {
+      eith <- fEith
+    } yield {
+      eith match {
+        case Right(x) => println(x)
+        case Left(x)  => println(x)
+      }
+    }
   }
 
 }
