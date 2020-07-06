0eee5a62cf2aa5eb0d4296e446bfa3e621c4a374
Fuma_Tsuchiya <korezonzip@gmail.com>
2020-07-06 16:29:30 +0900

    add example pra4

--- src/main/scala/optionProblem.scala
+++ src/main/scala/optionProblem.scala
@@ -3,10 +3,16 @@ object optionProblem {
     println(optionQuestion1(Some(2)))
     println(optQ2(Some(3)))
     println(optQ3)
-    println(optQ4(Some(Some(2))))
-    println(optQ4(Some(None)))
+    println(optQ4(optNumSome))
+    println(optQ4(optNumNone))
     println(optQ4(None))
+    println(opt04_2(optNumSome))
+    println(opt04_2(optNumNone))
+    println(opt04_2(None))
   }
+
+  val optNumSome = Some(Some(2))
+  val optNumNone = Some(None)
 
   def optionQuestion1(num: Option[Int]): Int = {
     num.getOrElse(0)
@@ -27,8 +33,13 @@ object optionProblem {
 
   def optQ4(num: Option[Option[Int]]): Option[Int] = {
     num match {
-      case Some(x) => x.map(y => y *2)
+      case Some(x) => x.map(_ * 2)
       case None    => None
     }
+  }
+  //Some(None): case None に入る
+
+  def opt04_2(num: Option[Option[Int]])= {
+    num.flatMap(_.map(_ * 2))
   }
 }
