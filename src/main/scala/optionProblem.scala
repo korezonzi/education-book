538eadcc297917262c6dac551132fd109149c1a3
Fuma Tsuchiya <fuma.tsuchiya@nextbeat.net>
2020-08-27 17:15:18 +0900

    add probrem statement

--- src/main/scala/optionProblem.scala
+++ src/main/scala/optionProblem.scala
@@ -17,13 +17,14 @@ object optionProblem {
   val optNumSome = Some(Some(2))
   val optNumNone = Some(None)
 
-  //Option[Int]型である、numが引数として渡されるメソッドoptionQuestion1があります。Someの場合はその値を、Noneの場合は０を返却するメソッドを、getOrElseメソッドを使って作成してください。
+  //Option[Int]型である、numが引数として渡されるメソッドoptionQuestion1があります。
+  // Someの場合はその値を、Noneの場合は０を返却するメソッドを、getOrElseメソッドを使って作成してください。
   def optionQuestion1(num: Option[Int]): Int = {
     num.getOrElse(0)
   }
 
-  /*Option[Int]型である、numが引数として渡されるoptionQuestion2メソッドがあります。
-  Someの場合はその値に3を足したSome[Int]を、Noneの場合はそのままNoneを返却するメソッドを、mapメソッドを使って作成してください。Option[Int]型である、numが引数として渡されるoptionQuestion2メソッドがあります。Someの場合はその値に3を足したSome[Int]を、Noneの場合はそのままNoneを返却するメソッドを、mapメソッドを使って作成してください。*/
+  //Option[Int]型である、numが引数として渡されるoptionQuestion2メソッドがあります。
+  //Someの場合はその値に3を足したSome[Int]を、Noneの場合はそのままNoneを返却するメソッドを、mapメソッドを使って作成してください。
   def optQ2(num: Option[Int]): Option[Int] = {
     num match {
       case Some(x) => Some(x).map(x => x+3)
@@ -31,14 +32,15 @@ object optionProblem {
     }
   }
 
-  //val strOpt: Option[String] = Some("") を定義し、それに対してisEmpty, isDefined を実行した場合にどのような挙動をするか確かめてください。val strOpt: Option[String] = Some("") を定義し、それに対してisEmpty, isDefined を実行した場合にどのような挙動をするか確かめてください。
+  //val strOpt: Option[String] = Some("") を定義し、それに対してisEmpty, isDefined を実行した場合にどのような挙動をするか確かめてください。
   def optQ3: Unit = {
     val strOpt: Option[String] = Some("")
     println(strOpt.isEmpty) //false
     println(strOpt.isDefined) //true
   }
 
-  //Option[Option[Int]]型である、numが引数として渡されるoptionQuestion4メソッドがあります。Someの場合はその値を2倍したSome[Int]を、Noneの場合はそのままNoneを返却するメソッドを作成してください。Option[Option[Int]]型である、numが引数として渡されるoptionQuestion4メソッドがあります。Someの場合はその値を2倍したSome[Int]を、Noneの場合はそのままNoneを返却するメソッドを作成してください。
+  //Option[Option[Int]]型である、numが引数として渡されるoptionQuestion4メソッドがあります。
+  // Someの場合はその値を2倍したSome[Int]を、Noneの場合はそのままNoneを返却するメソッドを作成してください。
   def optQ4(num: Option[Option[Int]]): Option[Int] = {
     num match {
       case Some(x) => x.map(_ * 2)
@@ -47,21 +49,20 @@ object optionProblem {
   }
   //Some(None): case None に入る
 
-  def opt04_2(num: Option[Option[Int]])= {
+  def opt04_2(num: Option[Option[Int]]): Option[Int]= {
     num.flatMap(_.map(_ * 2))
   }
 
-  def opt04_3(num: Option[Option[Int]]) = {
+  def opt04_3(num: Option[Option[Int]]): Option[Int] = {
     num.flatten.map(_ * 2)
   }
   //Some(None): flattenでNoneになる
 
   //yield: 要素加工して、Optionで包んで返す
-  def opt04_4(num: Option[Option[Int]]) = {
+  def opt04_4(num: Option[Option[Int]]): Option[Int] = {
     for {
       optInt <- num
       int    <- optInt
     }yield int * 2
   }
-
 }
